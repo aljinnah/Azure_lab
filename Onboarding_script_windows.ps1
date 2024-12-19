2e527ad027d07b7cc975b269f5223c0ec0d005b1
aljinnah <aljinnah87@gmail.com>
2024-12-19 15:39:40 -0600

    Onboarding script added
    It has a fake secret key that need to be updated

---
+++ Onboarding_script_windows.ps1
@@ -0,0 +1,49 @@
+$global:scriptPath = $myinvocation.mycommand.definition
+
+function Restart-AsAdmin {
+    $pwshCommand = "powershell"
+    if ($PSVersionTable.PSVersion.Major -ge 6) {
+        $pwshCommand = "pwsh"
+    }
+
+    try {
+        Write-Host "This script requires administrator permissions to install the Azure Connected Machine Agent. Attempting to restart script with elevated permissions..."
+        $arguments = "-NoExit -Command `"& '$scriptPath'`""
+        Start-Process $pwshCommand -Verb runAs -ArgumentList $arguments
+        exit 0
+    } catch {
+        throw "Failed to elevate permissions. Please run this script as Administrator."
+    }
+}
+
+try {
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        if ([System.Environment]::UserInteractive) {
+            Restart-AsAdmin
+        } else {
+            throw "This script requires administrator permissions to install the Azure Connected Machine Agent. Please run this script as Administrator."
+        }
+}
+    $ServicePrincipalId="78e3e942-67db-46fd-a6be-191cda020b88";
+    $ServicePrincipalClientSecret="rk32M~p5q2EH-qhhI_Q~fJFmvCgCWeEegZcEfalX";
+
+    $env:SUBSCRIPTION_ID = "97dc82f4-458e-4ff1-a935-2b799d2082f6";
+    $env:RESOURCE_GROUP = "ArcBox";
+    $env:TENANT_ID = "4cfe372a-37a4-44f8-91b2-5faf34253c62";
+    $env:LOCATION = "centralus";
+    $env:AUTH_TYPE = "principal";
+    $env:CORRELATION_ID = "9c682338-657d-45aa-9a03-898ccb90f4f3";
+    $env:CLOUD = "AzureCloud";
+    
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1";
+    & "$env:TEMP\install_windows_azcmagent.ps1";
+    if ($LASTEXITCODE -ne 0) { exit 1; }
+    & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id "$ServicePrincipalId" --service-principal-secret "$ServicePrincipalClientSecret" --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --correlation-id "$env:CORRELATION_ID";
+}
+catch {
+    $logBody = @{subscriptionId="$env:SUBSCRIPTION_ID";resourceGroup="$env:RESOURCE_GROUP";tenantId="$env:TENANT_ID";location="$env:LOCATION";correlationId="$env:CORRELATION_ID";authType="$env:AUTH_TYPE";operation="onboarding";messageType=$_.FullyQualifiedErrorId;message="$_";};
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/log" -Method "PUT" -Body ($logBody | ConvertTo-Json) | out-null;
+    Write-Host  -ForegroundColor red $_.Exception;
+}
